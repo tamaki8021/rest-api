73046c6f89542929e970f40481360f5bcfc53c45
tamaki <hbk2002320@gmail.com>
2021-04-04 16:40:33 +0900

    feat

--- app/app.js
+++ app/app.js
@@ -1,121 +1,178 @@
-const express = require('express')
-const app = express()
-const sqlite3 = require('sqlite3')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const dbPath = "app/db/database.splite3"
+const dbPath = "app/db/database.splite3";
 
 // リクエストのbodyをバースする設定
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // 静的ファイルのルートディレクトリを設定
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 // Get all users
-app.get('/api/v1/users', (req,res) => {
+app.get("/api/v1/users", (req, res) => {
   //connect database
-  const db = new sqlite3.Database(dbPath)
+  const db = new sqlite3.Database(dbPath);
 
-  db.all('SELECT * FROM users', (err, rows) => {
-    res.json(rows)
-  })
+  db.all("SELECT * FROM users", (err, rows) => {
+    res.json(rows);
+  });
 
-  db.close()
-})
+  db.close();
+});
 
-
-// Get a user
-app.get('/api/v1/users/:id', (req,res) => {
+// Get all following users
+app.get("/api/v1/users/:id/following", (req, res) => {
   //connect database
-  const db = new sqlite3.Database(dbPath)
-
-  // idを取得
+  const db = new sqlite3.Database(dbPath);
   const id = req.params.id
 
-  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
-    res.json(row)
-  })
+  db.all(`SELECT * FROM following LIFT JOIN users ON following.followed_id = users.id WHERE following_id = ${id}`, (err, rows) => {
+    if (!rows) {
+      res.status(404).send({error: "Not Found"})
+    } else {
+      res.status(200).json(rows);
+    }
+  });
 
-  db.close()
-})
+  db.close();
+});
+
+// Get a user
+app.get("/api/v1/users/:id", (req, res) => {
+  //connect database
+  const db = new sqlite3.Database(dbPath);
+  // idを取得
+  const id = req.params.id;
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    if (!row) {
+      res.status(404).send({ error: "Not Found!" });
+    } else {
+      res.status(200).json(row);
+    }
+  });
+
+  db.close();
+});
 
 // Search users matching keyword
-app.get('/api/v1/search', (req,res) => {
+app.get("/api/v1/search", (req, res) => {
   //connect database
-  const db = new sqlite3.Database(dbPath)
+  const db = new sqlite3.Database(dbPath);
 
   // 検索valueを取得
-  const keyword = req.query.q
+  const keyword = req.query.q;
 
   db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
-    res.json(rows)
-  })
-  db.close()
-})
+    res.json(rows);
+  });
+  db.close();
+});
 
-const run = async (sql, db, res, message) => {
+const run = async (sql, db) => {
   return new Promise((resolve, reject) => {
     db.run(sql, (err) => {
       if (err) {
-        res.status(500).send(err)
-        return reject()
+        return reject(err);
       } else {
-        res.json({message: message})
-        return resolve()
+        return resolve();
       }
-    })
-  })
-}
+    });
+  });
+};
 
 // Create a new user
-app.post('/api/v1/users', async (req, res) => {
-  // connect database 
-  const db = new sqlite3.Database(dbPath)
+app.post("/api/v1/users", async (req, res) => {
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({ error: "ユーザー名が指定荒れていません" });
+  } else {
+    // connect database
+    const db = new sqlite3.Database(dbPath);
 
-  const name = req.body.name
-  const profile = req.body.profile ? req.body.body
-  .profile : ""
-  const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : ""
+    const name = req.body.name;
+    const profile = req.body.profile ? req.body.profile : "";
+    const dateOfBirth = req.body.date_of_birth
+      ? req.body.body.date_of_birth
+      : "";
 
+    try {
+      await run(
+        `INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`,
+        db
+      );
+      res.status(201).send({ message: "新規ユーザーを作成しました" });
+    } catch (e) {
+      res.status(500).send({ error: e });
+    }
 
-  await run(`INSERT INTO users (name, profile, date_of_birth) VALUES ("${name}", "${profile}", "${dateOfBirth}")`, db, res, "新規ユーザーを作成しました！")
-
-  db.close()
-
-})
+    db.close();
+  }
+});
 
 // Update a user
-app.post('/api/v1/users/:id', async (req, res) => {
-  // connect database 
-  const db = new sqlite3.Database(dbPath)
-  const id = req.params.id
+app.put("/api/v1/users/:id", async (req, res) => {
+  if (!req.body.name || req.body.name === "") {
+    res.status(400).send({ error: "ユーザー名が指定荒れていません" });
+  } else {
+    // connect database
+    const db = new sqlite3.Database(dbPath);
+    const id = req.params.id;
+
+    // 現在のユーザー情報を取得する
+    db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
+      if (!row) {
+        res.status(404).send(`error: '指定されたユーザーが見つかりません`);
+      } else {
+        const name = req.body.name ? req.body.name : row.name;
+        const profile = req.body.profile ? req.body.body.profile : row.profile;
+        const dateOfBirth = req.body.date_of_birth
+          ? req.body.date_of_birth
+          : row.date_of_birth;
+
+        try {
+          await run(
+            `UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`,
+            db
+          );
+          res.status(200).send({ message: "ユーザー情報を更新しました" });
+        } catch (e) {
+          res.status(500).send({ error: e });
+        }
+      }
+    });
+
+    db.close();
+  }
+});
+
+// Delete user data
+app.delete("/api/v1/users/:id", async (req, res) => {
+  // connect database
+  const db = new sqlite3.Database(dbPath);
+  const id = req.params.id;
 
   // 現在のユーザー情報を取得する
   db.get(`SELECT * FROM users WHERE id = ${id}`, async (err, row) => {
-    const name = req.body.name ? req.body.name : row.name
-    const profile = req.body.profile ? req.body.body
-    .profile : row.profile
-    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth
+    if (!row) {
+      res.status(404).send(`error: '指定されたユーザーが見つかりません`);
+    } else {
+      try {
+        await run(`DELETE FROM users WHERE id=${id}`, db);
+        res.status(200).send({ message: "ユーザー情報を削除しました" });
+      } catch (e) {
+        res.status(500).send({ error: e });
+      }
+    }
+  });
 
-    await run(`UPDATE users SET name="${name}", profile="${profile}", date_of_birth="${dateOfBirth}" WHERE id=${id}`, db, res, "ユーザー情報を更新しました！")
-  })
+  db.close();
+});
 
-  db.close()
-})
-
-// Delete user data
-app.post('/api/v1/users/:id', async (req, res) => {
-  // connect database 
-  const db = new sqlite3.Database(dbPath)
-  const id = req.params.id
-
-  await run(`DELETE FROM users WHERE id=${id}`, db, res, "ユーザー情報を削除しました！")
-
-  db.close()
-})
-
-const port = process.env.PORT || 8000
-app.listen(port)
-console.log("LIsten on port:" + port )
+const port = process.env.PORT || 8000;
+app.listen(port);
+console.log("LIsten on port:" + port);
